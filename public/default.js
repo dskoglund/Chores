9dbaa3b5be92c983701549eceb5cb469fb3091e0
David Skoglund <dskoggz@gmail.com>
2017-01-11 14:39:16 -0800

    Working on CRUD.

--- public/default.js
+++ public/default.js
@@ -8,13 +8,23 @@ function HomeController($scope, $window, choresData) {
 
   const vm = this
 
-  vm.userList = []
+  vm.childList = []
+  vm.newChild = { name: '', dailyChores:[] }
+  vm.create = create
 
-  choresData.loadUsers().then(chores => {
-    vm.userList = chores
-    console.log(vm.userList)
-  })
+  loadUsers()
 
+  function loadUsers() {
+    choresData.readAll()
+      .then(chores => {vm.childList = chores})
+      .catch(() => showError('Server Error: Unable to Load Chores'))
+  }
+
+  function create(child) {
+    choresData.createChild(child)
+      .then(res => vm.chores.push(res) && (vm.newChild.name = ''))
+      .catch(() => showError('Server Error: Unable to Create Child'))
+  }
 }
 
 app.factory('choresData', choresData)
@@ -22,10 +32,24 @@ choresData.$inject = ['$http']
 function choresData($http) {
 
   return {
-    loadUsers
+    createChild,
+    // createChore,
+    readAll,
+    // updateChore,
+    // deleteChore,
+    // deleteChild
   }
 
-  function loadUsers() {
+  function createChild(item) {
+      return $http.post(URL, item).then(res => data)
+  }
+  //
+  // function createChore() {
+  //   return $http.post('./chore/')
+  // }
+
+  function readAll() {
     return $http.get('./chores').then(res => res.data)
   }
+
 }
--- routes.js
+++ routes.js
@@ -6,10 +6,20 @@ const appRouter = function(db) {
   const router = new Router()
 
   router.get('/', (req, res) => {
-    chores.find().toArray((err, docs) => {
+    chores
+      .find()
+      .toArray((err, docs) => {
       if(err) return next(err)
       res.json(docs)
-    })
+      })
+  })
+
+  router.post('/', (req,res,next) => {
+    let child = parse(req.body)
+    chores
+      .insertOne(child)
+      .then(() => res.status(201).json(child))
+      .catch(next)
   })
 
   return router
